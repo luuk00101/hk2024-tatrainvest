3b034fcf7b0cd416339fe923235380f96a0889e6
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-04-06 13:38:34 +0200

    Initial Commit

---
+++ hk2024-tatrainvest/hk2024_tatrainvestApp.swift
@@ -0,0 +1,17 @@
+//
+//  hk2024_tatrainvestApp.swift
+//  hk2024-tatrainvest
+//
+//  Created by Lukáš Kollárovič on 06/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct hk2024_tatrainvestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
